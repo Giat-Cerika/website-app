dc7ed8b526b9743caf24bf9e04b48e3a8429deff
jmsrzk14 <jmsrizky@gmail.com>
2025-12-24 15:20:31 +0700

    fix tlm

--- src/app/admin/tlm/page.tsx
+++ src/app/admin/tlm/page.tsx
@@ -187,7 +187,7 @@ export default function CariesRiskForm() {
     };
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/predict", {
+      const res = await fetch("https://web-production-b2442.up.railway.app/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
